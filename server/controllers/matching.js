4e6245c82a835ebbf3ebc5329591c109624861f9
Jesus <jesuss07@ucm.es>
2021-04-17 17:27:55 +0200

    Matching funcional y cambia bd

--- server/controllers/matching.js
+++ server/controllers/matching.js
@@ -739,7 +739,7 @@ function hacerMatch(fichero,oferta, demanda){
         console.log(valores);
         return matchDefinitivo(oferta, demanda, valores[0], valores[1], valores[2], valores[3], valores[4]).then(function(res){
             if(res >= 0.5){
-                console.log("Es match");//hacer funcion en DAO para insertar en tabla matching
+                return dao_tentativa.crearMatch(oferta.getId(), demanda.getId(), res)//hacer funcion en DAO para insertar en tabla matching
             }
             else {
                 console.log("No es match");
