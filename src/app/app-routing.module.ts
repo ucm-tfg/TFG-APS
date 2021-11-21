ea1b9b7f850d55e8389e7e72f7bd83d7df138011
Jonathan Moyonero <moyonero@gmail.com>
2021-11-21 14:54:00 +0200

    NavMenu actualizado y componente crear oferta actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -74,149 +74,157 @@ import {Code404Component} from './errors/code404/code404.component';
 
 const routes: Routes = [
 
-  // aplico authguard a todos los paths para intentar obtener el token siempre
-  {
-    path: '', canActivate: [AuthGuard], children: [
+    // aplico authguard a todos los paths para intentar obtener el token siempre
+    {
+        path: '', canActivate: [AuthGuard], children: [
 
-      // login routes
-      {path: 'login', canActivate: [GuestGuard], component: LoginComponent, data: {titulo: 'Login'}},
-      {path: 'registro', canActivate: [GuestGuard], component: RegisterComponent, data: {titulo: 'Registro'}},
-      {path: 'perfil', canActivate: [ProtectedGuard], component: ProfileComponent, data: {titulo: 'Perfil'}},
+            // login routes
+            {path: 'login', canActivate: [GuestGuard], component: LoginComponent, data: {titulo: 'Login'}},
+            {path: 'registro', canActivate: [GuestGuard], component: RegisterComponent, data: {titulo: 'Registro'}},
+            {path: 'perfil', canActivate: [ProtectedGuard], component: ProfileComponent, data: {titulo: 'Perfil'}},
 
-      // gestor routes
-      {
-        path: 'gestor', canActivate: [GestorGuard], children: [
-          {
-            path: 'gestion-usuarios',
-            component: GestionUsuariosComponent,
-            data: {titulo: 'Zona admin - Gestión de Usuarios'}
-          },
-          {
-            path: 'gestion-usuarios/editar/:uid',
-            component: GestionUsuariosEditarComponent,
-            data: {titulo: 'Zona admin - Editar Usuario'}
-          },
-          {path: 'gestion-emails', component: GestorEmailsComponent, data: {titulo: 'Zona admin - Gestión de Emails'}},
-          {
-            path: 'gestion-suscripciones',
-            component: GestorNewsletterComponent,
-            data: {titulo: 'Zona admin - Gestión de Suscripciones a newsletter'}
-          },
+            // gestor routes
+            {
+                path: 'gestor', canActivate: [GestorGuard], children: [
+                    {
+                        path: 'gestion-usuarios',
+                        component: GestionUsuariosComponent,
+                        data: {titulo: 'Zona admin - Gestión de Usuarios'}
+                    },
+                    {
+                        path: 'gestion-usuarios/editar/:uid',
+                        component: GestionUsuariosEditarComponent,
+                        data: {titulo: 'Zona admin - Editar Usuario'}
+                    },
+                    {
+                        path: 'gestion-emails',
+                        component: GestorEmailsComponent,
+                        data: {titulo: 'Zona admin - Gestión de Emails'}
+                    },
+                    {
+                        path: 'gestion-suscripciones',
+                        component: GestorNewsletterComponent,
+                        data: {titulo: 'Zona admin - Gestión de Suscripciones a newsletter'}
+                    },
+                ]
+            },
+
+            // pages routes
+            {path: '', component: HomeComponent, data: {titulo: 'Portal ApS'}},
+            {path: 'que-es-ApS', component: QueEsApsComponent, data: {titulo: '¿Qué es ApS?'}},
+
+            {
+                path: 'como-participar/estudiantes',
+                component: ComoParticiparEstudiantesComponent,
+                data: {titulo: 'Cómo participar - Estudiantes'}
+            },
+            {
+                path: 'como-participar/profesores',
+                component: ComoParticiparProfesoresComponent,
+                data: {titulo: 'Cómo participar - Profesores'}
+            },
+            {
+                path: 'como-participar/socioscomunitarios',
+                component: ComoParticiparSociosComunitariosComponent,
+                data: {titulo: 'Cómo participar - Socios comunitarios'}
+            },
+
+            {
+                path: 'sobre-ApS-UNED/quienes-somos',
+                component: SobreApsUnedQuienesSomosComponent,
+                data: {titulo: 'Sobre ApS UNED - Quiénes Somos'}
+            },
+            {
+                path: 'sobre-ApS-UNED/historia',
+                component: SobreApsUnedHistoriaComponent,
+                data: {titulo: 'Sobre ApS UNED - Historia'}
+            },
+            {
+                path: 'sobre-ApS-UNED/contacta',
+                component: SobreApsUnedContactaComponent,
+                data: {titulo: 'Sobre ApS UNED - Contacta'}
+            },
+
+            // Iniciativas
+            {path: 'iniciativas', component: IniciativasComponent, data: {titulo: 'Iniciativas'}},
+            {path: 'mis-iniciativas', component: IniciativasComponent, data: {titulo: 'Mis Iniciativas'}},
+            {path: 'iniciativas/ver/:id', component: IniciativasVerComponent, data: {titulo: 'Ver Iniciativa'}},
+            {
+                path: 'iniciativas/editar/:id',
+                canActivate: [IniciativaEditarGuard],
+                component: IniciativasEditarComponent,
+                data: {titulo: 'Editar Iniciativa'}
+            },
+            {
+                path: 'iniciativas/crear',
+                canActivate: [IniciativaCrearGuard],
+                component: IniciativasCrearComponent,
+                data: {titulo: 'Crear Iniciativa'}
+            },
+
+            // Ofertas
+            // { path: 'mis-ofertas', component: OfertasComponent, data: { titulo: 'Mis Ofertas' } },
+            // { path: 'ofertas/ver/:id', component: OfertasVerComponent, data: { titulo: 'Ver Oferta' } },
+            // { path: 'ofertas/editar/:id', canActivate:[OfertaEditarGuard], component: OfertasEditarComponent, data: { titulo: 'Editar Oferta' } },
+            {
+                path: 'ofertas/crear',
+                canActivate: [OfertaCrearGuard],
+                component: OfertasCrearComponent,
+                data: {titulo: 'Crear Oferta'}
+            },
+            {path: 'ofertas', component: OfertasComponent, data: {titulo: 'Ofertas'}},
+
+            //demandas
+            {path: 'demandas', component: DemandasComponent, data: {titulo: 'Demandas'}},
+            {path: 'testdem', component: DemandasComponent, data: {titulo: 'Demandas'}},
+            // { path: 'mis-demandas', component: demandasComponent, data: { titulo: 'Mis Demandas' } },
+            // { path: 'demandas/ver/:id', component: DemandasVerComponent, data: { titulo: 'Ver Demandas' } },
+            // { path: 'demandas/editar/:id', canActivate:[DemandaEditarGuard], component: DemandaEditarComponent, data: { titulo: 'Editar Demanda' } },
+            {
+                path: 'demandas/crear',
+                canActivate: [DemandaCrearGuard],
+                component: crearDemandaComponent,
+                data: {titulo: 'Crear Demanda'}
+            },
+
+            // Partenariados
+            {path: 'partenariados', component: PartenariadosComponent, data: {titulo: 'Partenariados'}},
+            {path: 'mis-partenariados', component: PartenariadosComponent, data: {titulo: 'Mis Partenariados'}},
+            {
+                path: 'partenariados/ver/:id',
+                canActivate: [PartenariadoVerGuard],
+                component: PartenariadosVerComponent,
+                data: {titulo: 'Ver Partenariado'}
+            },
+            {
+                path: 'partenariados/crear',
+                canActivate: [PartenariadoCrearProfesorGuard],
+                component: PartenariadoCrearProfesorComponent,
+                data: {titulo: 'Crear Partenariado'}
+            },
+
+
+            // Proyectos
+            {path: 'proyectos', component: ProyectosComponent, data: {titulo: 'Proyectos'}},
+            {path: 'mis-proyectos', component: ProyectosComponent, data: {titulo: 'Mis Proyectos'}},
+            {path: 'proyectos/ver/:id', component: ProyectosVerComponent, data: {titulo: 'Ver Proyecto'}},
+
+            // varios
+            {
+                path: 'registro/condiciones-de-uso',
+                component: CondicionesComponent,
+                data: {titulo: 'Condiciones de uso'}
+            },
+            {path: '**', component: Code404Component, data: {titulo: 'Página no encontrada'}},
         ]
-      },
-
-      // pages routes
-      {path: '', component: HomeComponent, data: {titulo: 'Portal ApS'}},
-      {path: 'que-es-ApS', component: QueEsApsComponent, data: {titulo: '¿Qué es ApS?'}},
-
-      {
-        path: 'como-participar/estudiantes',
-        component: ComoParticiparEstudiantesComponent,
-        data: {titulo: 'Cómo participar - Estudiantes'}
-      },
-      {
-        path: 'como-participar/profesores',
-        component: ComoParticiparProfesoresComponent,
-        data: {titulo: 'Cómo participar - Profesores'}
-      },
-      {
-        path: 'como-participar/socioscomunitarios',
-        component: ComoParticiparSociosComunitariosComponent,
-        data: {titulo: 'Cómo participar - Socios comunitarios'}
-      },
-
-      {
-        path: 'sobre-ApS-UNED/quienes-somos',
-        component: SobreApsUnedQuienesSomosComponent,
-        data: {titulo: 'Sobre ApS UNED - Quiénes Somos'}
-      },
-      {
-        path: 'sobre-ApS-UNED/historia',
-        component: SobreApsUnedHistoriaComponent,
-        data: {titulo: 'Sobre ApS UNED - Historia'}
-      },
-      {
-        path: 'sobre-ApS-UNED/contacta',
-        component: SobreApsUnedContactaComponent,
-        data: {titulo: 'Sobre ApS UNED - Contacta'}
-      },
-
-      // Iniciativas
-      {path: 'iniciativas', component: IniciativasComponent, data: {titulo: 'Iniciativas'}},
-      {path: 'mis-iniciativas', component: IniciativasComponent, data: {titulo: 'Mis Iniciativas'}},
-      {path: 'iniciativas/ver/:id', component: IniciativasVerComponent, data: {titulo: 'Ver Iniciativa'}},
-      {
-        path: 'iniciativas/editar/:id',
-        canActivate: [IniciativaEditarGuard],
-        component: IniciativasEditarComponent,
-        data: {titulo: 'Editar Iniciativa'}
-      },
-      {
-        path: 'iniciativas/crear',
-        canActivate: [IniciativaCrearGuard],
-        component: IniciativasCrearComponent,
-        data: {titulo: 'Crear Iniciativa'}
-      },
-
-      // Ofertas
-      // { path: 'mis-ofertas', component: OfertasComponent, data: { titulo: 'Mis Ofertas' } },
-      // { path: 'ofertas/ver/:id', component: OfertasVerComponent, data: { titulo: 'Ver Oferta' } },
-      // { path: 'ofertas/editar/:id', canActivate:[OfertaEditarGuard], component: OfertasEditarComponent, data: { titulo: 'Editar Oferta' } },
-      {
-        path: 'ofertas/crear',
-        canActivate: [OfertaCrearGuard],
-        component: OfertasCrearComponent,
-        data: {titulo: 'Crear Oferta'}
-      },
-      {path: 'ofertas', component: OfertasComponent, data: {titulo: 'Ofertas'}},
-
-      //demandas
-      {path: 'demandas', component: DemandasComponent, data: {titulo: 'Demandas'}},
-      {path: 'testdem', component: DemandasComponent, data: {titulo: 'Demandas'}},
-      // { path: 'mis-demandas', component: demandasComponent, data: { titulo: 'Mis Demandas' } },
-      // { path: 'demandas/ver/:id', component: DemandasVerComponent, data: { titulo: 'Ver Demandas' } },
-      // { path: 'demandas/editar/:id', canActivate:[DemandaEditarGuard], component: DemandaEditarComponent, data: { titulo: 'Editar Demanda' } },
-      {
-        path: 'demandas/crear',
-        canActivate: [DemandaCrearGuard],
-        component: crearDemandaComponent,
-        data: {titulo: 'Crear Demanda'}
-      },
-
-      // Partenariados
-      {path: 'partenariados', component: PartenariadosComponent, data: {titulo: 'Partenariados'}},
-      {path: 'mis-partenariados', component: PartenariadosComponent, data: {titulo: 'Mis Partenariados'}},
-      {
-        path: 'partenariados/ver/:id',
-        canActivate: [PartenariadoVerGuard],
-        component: PartenariadosVerComponent,
-        data: {titulo: 'Ver Partenariado'}
-      },
-      {
-        path: 'partenariados/crear',
-        canActivate: [PartenariadoCrearProfesorGuard],
-        component: PartenariadoCrearProfesorComponent,
-        data: {titulo: 'Crear Partenariado'}
-      },
-
-
-      // Proyectos
-      {path: 'proyectos', component: ProyectosComponent, data: {titulo: 'Proyectos'}},
-      {path: 'mis-proyectos', component: ProyectosComponent, data: {titulo: 'Mis Proyectos'}},
-      {path: 'proyectos/ver/:id', component: ProyectosVerComponent, data: {titulo: 'Ver Proyecto'}},
-
-      // varios
-      {path: 'registro/condiciones-de-uso', component: CondicionesComponent, data: {titulo: 'Condiciones de uso'}},
-      {path: '**', component: Code404Component, data: {titulo: 'Página no encontrada'}},
-    ]
-  }
+    }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {useHash: true}),
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {useHash: true}),
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
